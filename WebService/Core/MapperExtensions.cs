2c5004a52f7e246164602e4ac9827a75ce2c28b1
Julien VANDENBUSSCHE <julien.vandenbussche@axa.fr>
2019-08-02 16:24:48 +0200

    refactor(targetFrameworks): change targetframeworks to netstandard2.0, netcoreapp2.2, net4.8

---
+++ WebService/Core/MapperExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace WebService.Core
+{
+    using AutoMapper;
+
+    using Roi.Domain.Commons.Models;
+
+    internal static class MapperExtensions
+    {
+        public static TDestination Map<TDestination, TNotFoundDestination>(this IMapper mapper, object source)
+            where TNotFoundDestination : TDestination
+        {
+            if (source is INotFound)
+            {
+                return mapper.Map<TNotFoundDestination>(source);
+            }
+
+            return mapper.Map<TDestination>(source);
+        }
+    }
+}
